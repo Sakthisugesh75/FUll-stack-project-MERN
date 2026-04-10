ace0a7541530186fdc54d90c73bd53dd455d0ace
Sakthisugesh <sakthisugeshraju75@gmail.com>
2026-04-10 19:33:03 +0530

    push

--- frontend/src/Pages/CardScreen.jsx
+++ frontend/src/Pages/CardScreen.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux"
-
+import { Link } from "react-router-dom";
 const CardScreen = () => {
 
 const dispatch=useDispatch();
@@ -12,7 +12,29 @@ console.log(cartItem);
           <h1 className="text-5xl font-bold mb-10 text-center">
             MY CART
           </h1>
-          
+
+          {cartItem.length === 0 ? (
+            <div className="alert alert-warning shadow-lg">
+              <div>
+                <span className="text -lg"> There is no data here</span></div>
+            </div>
+          ):(
+            <div>
+              {cartItem.map((item)=>(
+                <div className="grid grid-cols-3 gap-5 items-center" key={item._id}>
+                  <div className="col-span-1">
+                    <Link>
+                    <img src={item.image} alt={item.name}>
+                    </img>
+                    
+                    </Link>
+                    <div className="text-lg">{item.name}</div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          ) 
+        }
       </div>
 
 
